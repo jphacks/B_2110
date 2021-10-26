6264cbac02329ff292e3c845793e484b16a866a1
Sota Kudo <sotakudo@ws20is070.naist.jp>
2021-10-26 21:53:35 +0900

    core ML (food11でエラー)

--- jphack_b2110/jphack_b2110/CameraViewController.swift
+++ jphack_b2110/jphack_b2110/CameraViewController.swift
@@ -1,121 +1,64 @@
 import UIKit
-import AVFoundation
 import CoreML
 import Vision
-
-class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+ 
+ 
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate{
+    
+    @IBOutlet var cameraView : UIImageView!
+    @IBOutlet var label : UILabel!
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-       // setupCaptureSession()
-      //  setupDevice()
-      //  setupInputOutput()
-      //  setupPreviewLayer()
-      //  captureSession.startRunning()
-    }
-
-    // デバイスからの入力と出力を管理するオブジェクトの作成
-    var captureSession = AVCaptureSession()
-
-    // カメラの画質の設定
-    func setupCaptureSession() {
-        captureSession.sessionPreset = AVCaptureSession.Preset.photo
     }
     
-    // カメラデバイスそのものを管理するオブジェクトの作成
-    // メインカメラの管理オブジェクトの作成
-    var mainCamera: AVCaptureDevice?
-    // インカメの管理オブジェクトの作成
-    var innerCamera: AVCaptureDevice?
-    // 現在使用しているカメラデバイスの管理オブジェクトの作成
-    var currentDevice: AVCaptureDevice?
-    
-    // デバイスの設定
-    func setupDevice() {
-        // カメラデバイスのプロパティ設定
-        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
-        // プロパティの条件を満たしたカメラデバイスの取得
-        let devices = deviceDiscoverySession.devices
-
-        for device in devices {
-            if device.position == AVCaptureDevice.Position.back {
-                mainCamera = device
-            } else if device.position == AVCaptureDevice.Position.front {
-                innerCamera = device
-            }
+    // カメラの撮影開始
+    @IBAction func startCamera(_ sender : Any) {
+        
+        let sourceType:UIImagePickerController.SourceType =
+            UIImagePickerController.SourceType.camera
+ 
+        // カメラが利用可能かチェック
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerController.SourceType.camera){
+            // インスタンスの作成
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.sourceType = sourceType
+            cameraPicker.delegate = self
+            self.present(cameraPicker, animated: true, completion: nil)
+            
         }
-        // 起動時のカメラを設定
-        currentDevice = mainCamera
-    }
-    
-    // キャプチャーの出力データを受け付けるオブジェクト
-    var photoOutput : AVCapturePhotoOutput?
-
-    // 入出力データの設定
-    func setupInputOutput() {
-        do {
-            // 指定したデバイスを使用するために入力を初期化
-            let captureDeviceInput = try AVCaptureDeviceInput(device: currentDevice!)
-            // 指定した入力をセッションに追加
-            captureSession.addInput(captureDeviceInput)
-            // 出力データを受け取るオブジェクトの作成
-            photoOutput = AVCapturePhotoOutput()
-            // 出力ファイルのフォーマットを指定
-            photoOutput!.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey : AVVideoCodecType.jpeg])], completionHandler: nil)
-            captureSession.addOutput(photoOutput!)
-        } catch {
-            print(error)
+        else{
+            label.text = "error"
         }
     }
     
-    // プレビュー表示用のレイヤ
-    var cameraPreviewLayer : AVCaptureVideoPreviewLayer?
-
-    // カメラのプレビューを表示するレイヤの設定
-    func setupPreviewLayer() {
-        // 指定したAVCaptureSessionでプレビューレイヤを初期化
-        self.cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        // プレビューレイヤが、カメラのキャプチャーを縦横比を維持した状態で、表示するように設定
-        self.cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        // プレビューレイヤの表示の向きを設定
-        self.cameraPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
-
-        self.cameraPreviewLayer?.frame = view.frame
-        self.view.layer.insertSublayer(self.cameraPreviewLayer!, at: 0)
-    }
-    /// UIImagePickerController カメラを起動する
-    /// - Parameter sender: "UIImagePickerController"ボタン
-
-    @IBAction func CameraBotton(_ sender: Any) {
-        // カメラで撮影した後の画像を予測に送る
-    }
-    
-    @IBAction func Camera(_ sender: UIButton) {
-        let picker = UIImagePickerController()
-        picker.sourceType = .camera
-        picker.delegate = self
-        // UIImagePickerController カメラを起動する
-        present(picker, animated: true, completion: nil)
-    }
-    /// シャッターボタンを押下した際、確認メニューに切り替わる
-    /// - Parameters:
-    ///   - picker: ピッカー
-    ///   - info: 写真情報
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let image = info[.originalImage] as! UIImage
-        // "写真を使用"を押下した際、写真アプリに保存する
-        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
-        // UIImagePickerController カメラが閉じる
-        self.dismiss(animated: true, completion: nil)
-        photoPredict(image)
+    //　撮影が完了時した時に呼ばれる
+    func imagePickerController(_ imagePicker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        
+        // dismiss
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+        if let pickedImage = info[.originalImage]
+            as? UIImage {
+            
+            cameraView.contentMode = .scaleAspectFit
+            cameraView.image = pickedImage
+            
+            photoPredict(pickedImage)
+        }
     }
     
     func photoPredict(_ targetPhoto: UIImage){
+        
         // 学習モデルのインスタンス生成
-        guard let model = try? VNCoreMLModel(for: Food11().model) else{
-               print("error model")
-               return
-           }
+        guard let model = try? VNCoreMLModel(for: Resnet50().model) else{
+            print("error model")
+            return
+        }
+        
         // リクエスト
         let request = VNCoreMLRequest(model: model){
             request, error in
@@ -128,10 +71,10 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
             let name = results[0].identifier
             
             if conf >= 50{
-               // self.label.text = "\(name) です。確率は\(conf)% \n"
+                self.label.text = "\(name) です。確率は\(conf)% \n"
             }
             else{
-              //  self.label.text = "もしかしたら、\(name) かも。確率は\(conf)% \n"
+                self.label.text = "もしかしたら、\(name) かも。確率は\(conf)% \n"
             }
         }
         
@@ -158,5 +101,63 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         }catch {
             print("error handler")
         }
+        
     }
+ 
+    // 撮影がキャンセルされた時に呼ばれる
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    // 写真を保存
+    @IBAction func savePicture(_ sender : Any) {
+        let image:UIImage! = cameraView.image
+        
+        if image != nil {
+            UIImageWriteToSavedPhotosAlbum(
+                image,
+                self,
+                #selector(CameraViewController.image(_:didFinishSavingWithError:contextInfo:)),
+                nil)
+        }
+        else{
+            print("image Failed !")
+        }
+        
+    }
+    
+    // 書き込み完了結果の受け取り
+    @objc func image(_ image: UIImage,
+                     didFinishSavingWithError error: NSError!,
+                     contextInfo: UnsafeMutableRawPointer) {
+        
+        if error != nil {
+            print(error.code)
+        }
+        else{
+        }
+    }
+    
+    // アルバムを表示
+    @IBAction func showAlbum(_ sender : Any) {
+        let sourceType:UIImagePickerController.SourceType =
+            UIImagePickerController.SourceType.photoLibrary
+        
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerController.SourceType.photoLibrary){
+            // インスタンスの作成
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.sourceType = sourceType
+            cameraPicker.delegate = self
+            self.present(cameraPicker, animated: true, completion: nil)
+            
+        }
+        else{
+            label.text = "error"
+            
+        }
+        
+    }
+    
 }
