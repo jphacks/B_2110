abf8a8d73bff56e46428138f191d09765d8923d0
TAKUMI-TAKAMATSU <e175744@ie.u-ryukyu.ac.jp>
2021-10-27 23:42:34 +0900

    最後に食べた時間についての情報を保持する値を追加

--- jphack_b2110/jphack_b2110/CharacterViewController.swift
+++ jphack_b2110/jphack_b2110/CharacterViewController.swift
@@ -6,11 +6,16 @@
 //
 
 import Foundation
-//import RealmSwift
+import RealmSwift
 
-/*
+
 class User: Object {
     @objc dynamic var name:String = ""
+    @objc dynamic var lastEatTime:String = ""
     @objc dynamic var total_score: Int16  = 0
+    //最後に食べた時間の保持 多分stringにして保持するのが丸い
+    //@objc dynamic var last_eat_time:String = ""
+    //@objc dynamic var documentDirectoryFileURL
+    //= FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
 }
-*/
+
--- jphack_b2110/jphack_b2110/ViewController.swift
+++ jphack_b2110/jphack_b2110/ViewController.swift
@@ -6,10 +6,17 @@
 //
 
 import UIKit
-//import RealmSwift
+import RealmSwift
 class ViewController: UIViewController {
 
     @IBOutlet weak var hiyokoView: UIImageView!
+    @IBOutlet weak var ScoreText: UILabel!
+    
+    // ドキュメントディレクトリの「ファイルURL」（URL型）定義
+    var documentDirectoryFileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+
+    // ドキュメントディレクトリの「パス」（String型）定義
+    let filePath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,16 +25,18 @@ class ViewController: UIViewController {
         
         //hiyokoView.loadGif(name: "ikePiyo")
         hiyokoView.loadGif(name: "pochaPiyo")
-        /*
+        
         let User1 = User()
         User1.name = "Test"//追加処理の例
-        User1.total_score = 1
+        User1.total_score += 2
         
             let realm = try! Realm()
             try! realm.write{//データベースへの書き込み処理
                 realm.add(User1)
               
-    }*/
+    }
+       // User1.total_score += 2
+        ScoreText.text = String(User1.total_score)
 }
 }
 
