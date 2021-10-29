23d6ce9b0066e7fa35da93ff186bcd47e083cf5b
TAKUMI-TAKAMATSU <e175744@ie.u-ryukyu.ac.jp>
2021-10-29 16:18:01 +0900

    データベースの変数定義

--- jphack_b2110/jphack_b2110/CharacterViewController.swift
+++ jphack_b2110/jphack_b2110/CharacterViewController.swift
@@ -12,5 +12,15 @@ import RealmSwift
 class User: Object {
     @objc dynamic var name:String = ""
     @objc dynamic var total_score: Int16  = 0
+    @objc dynamic var Bread: Double  = 0
+    @objc dynamic var Dairy: Double = 0
+    @objc dynamic var Egg: Double  = 0
+    @objc dynamic var Fried: Double  = 0
+    @objc dynamic var Meat: Double  = 0
+    @objc dynamic var Noodles: Double  = 0
+    @objc dynamic var Rice: Double  = 0
+    @objc dynamic var Seafood: Double  = 0
+    @objc dynamic var Soup: Double  = 0
+    @objc dynamic var Vegetable: Double  = 0
 }
 
