98589870806413e595471b071c6b519bddfab276
TAKUMI-TAKAMATSU <e175744@ie.u-ryukyu.ac.jp>
2021-10-30 09:45:37 +0900

    ViewControllerリファクタリング

--- jphack_b2110/jphack_b2110/ViewController.swift
+++ jphack_b2110/jphack_b2110/ViewController.swift
@@ -17,38 +17,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        
-        // (1)Realmのインスタンスを生成する
-        let realm = try! Realm()
-        // (2)全データの取得
-        let Foods = realm.objects(Food.self)
-        // (3)取得データの確認
-       // print(Food)
-        if (Foods.count > 0){
-            // 取得件数の表示
-            print(Foods.count)
-            let bird_status : Float = Foods[Foods.count].calories + Foods[Foods.count].fat + Foods[Foods.count].carbohydrate + Foods[Foods.count].protein + Foods[Foods.count].vitamin
-            
-            
-            if ( bird_status > 100){
-                hiyokoView.loadGif(name: "pochaPiyo")
-            } else if (bird_status > 50){
-                hiyokoView.loadGif(name: "ikePiyo")
-            }else{
-                hiyokoView.loadGif(name: "yasePiyo")
-            }
-        }else{
-            hiyokoView.loadGif(name: "yasePiyo")
-            hiyokoTalkText.text = "おなかすいた！"
-        }
-        
+        BirdStateCheck()
 }
     
     override func viewWillAppear(_ animated: Bool) {//画面に遷移したときに実行される。
         super.viewWillAppear(animated)
         print("viewWillAppear")
+        BirdStateCheck()
 
+   }
+    
+    func BirdStateCheck(){
         // (1)Realmのインスタンスを生成する
         let realm = try! Realm()
         // (2)全データの取得
@@ -74,10 +53,9 @@ class ViewController: UIViewController {
                 hiyokoTalkText.text = "草足りない!"
             }
         }else{
+            hiyokoView.loadGif(name: "yasePiyo")
             hiyokoTalkText.text = "おなかすいた！"
         }
-   }
-    
-    
+    }
 }
 
