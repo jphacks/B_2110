aebfecf7eda4f0113a35d0afcdbda439b802ef19
TAKUMI-TAKAMATSU <e175744@ie.u-ryukyu.ac.jp>
2021-10-30 09:22:00 +0900

    取得した値から状態の変化

--- jphack_b2110/jphack_b2110/ViewController.swift
+++ jphack_b2110/jphack_b2110/ViewController.swift
@@ -15,18 +15,57 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        if (a  == 1){
-            hiyokoView.loadGif(name: "pochaPiyo")
-        } else if (a == 0){
-            hiyokoView.loadGif(name: "yasePiyo")
-        }else{
+        
+        // (1)Realmのインスタンスを生成する
+        let realm = try! Realm()
+        // (2)全データの取得
+        let Foods = realm.objects(Food.self)
+        // (3)取得データの確認
+       // print(Food)
+        if (Foods.count > 0){
+            // 取得件数の表示
+            print(Foods.count)
+            let bird_status : Float = Foods[Foods.count].calories + Foods[Foods.count].fat + Foods[Foods.count].carbohydrate + Foods[Foods.count].protein + Foods[Foods.count].vitamin
             
+            
+            if ( bird_status > 100){
+                hiyokoView.loadGif(name: "pochaPiyo")
+            } else if (bird_status > 50){
+                hiyokoView.loadGif(name: "ikePiyo")
+            }else{
+                hiyokoView.loadGif(name: "yasePiyo")
+            }
+        }else{
+            hiyokoView.loadGif(name: "yasePiyo")
         }
+        
 }
     
     override func viewWillAppear(_ animated: Bool) {//画面に遷移したときに実行される。
         super.viewWillAppear(animated)
         print("viewWillAppear")
+
+        // (1)Realmのインスタンスを生成する
+        let realm = try! Realm()
+        // (2)全データの取得
+        let Foods = realm.objects(Food.self)
+        // (3)取得データの確認
+       // print(Food)
+        if (Foods.count > 0){
+            // 取得件数の表示
+            print(Foods.count)
+            let bird_status : Float = Foods[Foods.count].calories + Foods[Foods.count].fat + Foods[Foods.count].carbohydrate + Foods[Foods.count].protein + Foods[Foods.count].vitamin
+            
+            
+            if ( bird_status > 100){
+                hiyokoView.loadGif(name: "pochaPiyo")
+            } else if (bird_status > 50){
+                hiyokoView.loadGif(name: "ikePiyo")
+            }else{
+                hiyokoView.loadGif(name: "yasePiyo")
+            }
+        }
+        
     }
     
     
